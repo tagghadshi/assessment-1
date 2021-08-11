5e4f9bae69989532bbeb07edb763cc4ac4a04be6
tagghadshi <tagghadshi@gmail.com>
2021-08-11 12:38:43 +0000

    Savings and Current Accounts class

---
+++ src/com/tcs/bank/Account.java
@@ -0,0 +1,25 @@
+package com.tcs.bank;
+
+public class Account {
+	private String accountNo ;
+	private String accountType;
+	private String owner;
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	
+}
---
+++ src/com/tcs/bank/SavingsAccount.java
@@ -0,0 +1,43 @@
+package com.tcs.bank;
+
+import java.util.Date;
+
+public class SavingsAccount extends Account {
+	private double balance;
+	private final String type="savings";
+	private Date createDate;
+	private String status;
+	
+	
+	
+	public SavingsAccount(double balance, Date createDate, String status) {
+		super();
+		this.balance = balance;
+		this.createDate = createDate;
+		this.status = status;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getType() {
+		return type;
+	}
+	
+	
+}
