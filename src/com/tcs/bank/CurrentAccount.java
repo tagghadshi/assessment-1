8b2b11787c7186854f4bc1f59abd931e7d2e4356
tagghadshi <tagghadshi@gmail.com>
2021-08-11 12:53:50 +0000

    Added Bank class

--- src/com/tcs/bank/CurrentAccount.java
+++ src/com/tcs/bank/CurrentAccount.java
@@ -4,40 +4,45 @@ import java.util.Date;
 
 public class CurrentAccount {
 	private double balance;
-	private final String type="current";
+	private final String type = "current";
 	private Date createDate;
 	private String status;
 	private double overDraftBalance;
-	
-	
-	public CurrentAccount(double balance, Date createDate, String status,double overDraftBalance) {
+
+	public CurrentAccount(double balance, Date createDate, String status, double overDraftBalance) {
 		super();
 		this.balance = balance;
 		this.createDate = createDate;
 		this.status = status;
 		this.overDraftBalance = overDraftBalance;
 	}
+
 	public double getBalance() {
 		return balance;
 	}
+
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
+
 	public Date getCreateDate() {
 		return createDate;
 	}
+
 	public void setCreateDate(Date createDate) {
 		this.createDate = createDate;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public String getType() {
 		return type;
 	}
-	
-	
+
 }
